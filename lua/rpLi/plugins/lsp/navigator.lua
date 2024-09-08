7079a237acc7863e650d3752cd05a0521d66ad7f
lrppp <vsian_v@outlook.com>
2024-09-09 02:47:54 +0800

    rewrite signature help so no conflict with navigating splits

--- lua/rpLi/plugins/lsp/navigator.lua
+++ lua/rpLi/plugins/lsp/navigator.lua
@@ -1,7 +1,9 @@
 -- import mason plugin safely
 local navigator_status, navigator = pcall(require, "navigator")
 if not navigator_status then
-	return
+    return
 end
 
-navigator.setup()
+navigator.setup({
+    keymaps = { { key = "<c-y>", func = vim.lsp.buf.signature_help, desc = 'signature_help' } },
+})
